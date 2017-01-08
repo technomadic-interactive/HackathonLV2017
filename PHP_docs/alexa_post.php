7f3a8d8a17ced35e7eee9e88f764945cf66c99f4
Rogelio Ventura <rogeliovr94@gmail.com>
2017-01-08 09:45:06 -0800

    Update Alexa_post (Alejandro)

--- PHP_docs/alexa_post.php
+++ PHP_docs/alexa_post.php
@@ -1,26 +1,24 @@
 <?php
 //Header('application/x-www-form-urlencoded');
-if(isset($_GET['llamar'])){
-	if($_GET['llamar']=='true'){
+if(isset($_POST['llamar'])){
+	if($_POST['llamar']=='true'){
         	$f = fopen('flag.txt','wb')or die('error alexa l abrir el archivo');
         	fwrite($f,1);
         	fclose($f);
         	$f = fopen('log.txt','ab')or die('error alexa l abrir el archivo');
         	fwrite($f,date()."----viene del pos alexa true-----"." ".$llamar."\r\n");
         	fclose($f);
-
         	$output = shell_exec('php make_calls.php');
         	echo "<pre>$output</pre>";
         	exit();
 	}
-	if($_GET['llamar'] =='false'){
+	if($_POST['llamar'] =='false'){
         	$f = fopen('flag.txt','wb')or die('error alexa l abrir el archivo');
         	fwrite($f,0);
         	fclose($f);
         	$f = fopen('log.txt','ab')or die('error alexa l abrir el archivo');
         	fwrite($f,date()."-----viene del post alexa false-----"." ".$llamar."\r\n");
         	fclose($f);
-
 	}
 }
 else echo "no post llamar";
